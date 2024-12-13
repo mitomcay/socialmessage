88214fb8e4f84ecfa988e3e11603850a5f66e339
Quyền Đặng <ninicom206@gmail.com>
2024-12-13 15:45:14 +0700

    update api post

--- routes/api/post.js
+++ routes/api/post.js
@@ -3,10 +3,13 @@ var router = express.Router();
 const authMiddleware = require('../../middlewares/api/authMiddleware')
 var postcontroller = require('../../controllers/api/postcontroller');
 
-router.get('/', authMiddleware, postcontroller.getpost)
+router.get('/:postId', postcontroller.getpost)
 router.get('/getNewPost/:page/:limit', authMiddleware, postcontroller.getNewPost)
 router.post('/pushpost', authMiddleware, postcontroller.pushpost);
 router.get('/userpost/:userId', authMiddleware, postcontroller.getUserPost);
-router.get('/searchpost/:query/:page/:limit', authMiddleware, postcontroller.searchPost)
+router.get('/searchpost/:query/:page/:limit', authMiddleware, postcontroller.searchPost);
+router.post('/likePost/:postId', authMiddleware, postcontroller.likepost);
+router.delete('/unLikePost/:postId', authMiddleware, postcontroller.unlikepost);
+
 
 module.exports = router;
